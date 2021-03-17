8c136988ec99513c30fe4af01f22b2d4e9812aa0
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-03-17 12:56:54 +0000

    Add files via upload

---
+++ task1_triangle.java
@@ -0,0 +1,15 @@
+package day_March_17;
+
+public class task1_triangle {
+
+	public static void main(String[] args) {
+
+		System.out.println("    *");
+		System.out.println("   * *");
+		System.out.println("  *   *");
+		System.out.println(" *     *");
+		System.out.println("*********");
+
+	}
+
+}
---
+++ task4_escapeSequences.java
@@ -0,0 +1,15 @@
+package day_March_17;
+
+public class task4_escapeSequences {
+
+	public static void main(String[] args) {
+	
+		System.out.println("He said \"Hello\".");
+		
+		System.out.println("Would you like \'coffee\' or \'tea\' ?");
+		
+		System.out.println("\"\\__/\" ");
+
+	}
+
+}
