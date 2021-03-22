d03e1434b90eb395c34ef0361be830c67de0a9c4
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-03-22 22:20:17 +0000

    Add files via upload

---
+++ convertKiloToPound.java
@@ -0,0 +1,32 @@
+package assignments.day_4;
+
+public class convertKiloToPound {
+
+	public static void main(String[] args) {
+		
+		/* Write a program that can convert kg to pounds. 
+		hint: 1kg = 2.2 pounds
+ 		ex: if kg = 10, then in pounds it should be 22.0 pounds*/
+
+		double kg,pounds;
+		kg=10;
+		
+		pounds=kg*2.2;
+		
+		System.out.println(kg + " kg is equal to " + pounds + " pounds");
+		
+		double gram,kilo;
+		kilo=10;
+		
+		gram=kilo*1000;
+		
+		System.out.println(kilo + " kilo is equal to " + gram + " gram");
+		
+						
+		    
+		    }
+
+		
+		
+	}
+
