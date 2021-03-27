69243ee9f4be22b51ab76779e36b46abc7f93a32
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-03-27 12:56:53 +0000

    Add files via upload

---
+++ logicalOperators.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class logicalOperators {
+
+	public static void main(String[] args) {
+		
+	
+		System.out.println(((3<5) && !(1>14) && (-5<-15)) || ((6==6) && !(2==2)));
+		// T && T && F
+		// F || T && F > F || F >> F
+		
+	
+
+	}
+
+}
