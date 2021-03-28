4f4a4d36628824a7570ab9141fa57d03b8143c38
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-03-28 22:23:58 +0000

    Add files via upload

---
+++ logicalOperators2.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class logicalOperators2 {
+
+	public static void main(String[] args) {
+	
+		int a = 3;
+		int b = -2;
+		
+		System.out.println(!(a>0 || b>0 ));
+
+	}
+
+}
