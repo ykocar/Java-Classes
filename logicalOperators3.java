cd15a41c7f2e80bac87fddcb11175e119e190761
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-03-29 22:47:52 +0000

    Add files via upload

---
+++ logicalOperators3.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class logicalOperators3 {
+
+	public static void main(String[] args) {
+
+		int a=5, b=9;
+		boolean c = a>1 || b++<10;
+		System.out.println(b);
+
+
+	}
+
+}
