baa71faf16ef9e4aca821584c6a52287adc36d82
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-04-02 10:02:25 +0000

    Add files via upload

--- ternaryOperator.java
+++ ternaryOperator.java
@@ -5,22 +5,20 @@ public class ternaryOperator {
 	public static void main(String[] args) {
 	
 		
+		String result;
+		int score=88;
 		
-		
-		int x=1;
-		System.out.println(x>2 ? x<4 ? 10 : 8 : 7);
-		
-		if(x>2) {
-			if(x<4) {
-				System.out.println(10);
-			}else {
-				System.out.println(8);
-			}
-		}else {
-				System.out.println(7);
-			}
-			}
+		if(score>60) {
+			result = "pass";
 
-	}
+		}else {
+			result = "fail";
+			}
+		result = score>60 ? "pass" : "fail";
+		
+		System.out.println(result);
+ 	}
+
+}
 
 
