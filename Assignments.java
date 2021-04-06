bc74981cf246abd3fdc476949633734448bf80be
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-04-06 23:54:56 +0000

    Add files via upload

---
+++ Assignments.java
@@ -0,0 +1,21 @@
+package day12_BranchingStatements;
+
+public class Assignments {
+
+	public static void main(String[] args) {
+	
+		for(int i=0;i>-3;i--) {
+		
+		System.out.println(i);
+		}
+		
+		int j;
+		for(j=0;j<=2;j++) {
+			
+			
+		}
+		System.out.println(j);
+		
+	}
+
+}
