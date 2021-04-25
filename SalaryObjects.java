d22ec732f590c6f6b59f473be98ce018a3d0f463
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-04-25 22:20:42 +0000

    Add files via upload

---
+++ SalaryObjects.java
@@ -0,0 +1,28 @@
+package week5;
+
+public class SalaryObjects {
+
+	public static void main(String[] args) {
+	
+		Salary salary1= new Salary(); //employee1
+		salary1.setInfo(45, 40, 0.3);
+
+		System.out.println(salary1);
+		
+		Salary salary2 = new Salary(); // employee2: hourlyRate:$55, weeklyHour=45 hours
+		
+		salary2.setInfo(55, 45, 0.28);
+		
+		System.out.println(salary2); //toString method gets executed automatically
+		
+		System.out.println("####################################");
+		
+		// employee2 hourlyRate = 65;
+		
+		salary2.rate = 65;
+		
+		System.out.println(salary2);
+		
+	}
+
+}
--- calculator.java
+++ calculator.java
@@ -1,35 +1,74 @@
-package day8_controlFlowStatements_part2;
+package week5;
+
+import java.util.Scanner;
 
 public class calculator {
 
 	public static void main(String[] args) {
-	
-		int num1=10;
-		int num2=5;
-		String operator="*";
-		int result=0;
-		
-		switch(operator) {
-		
-		case "+":
-			result = num1+num2;
-			break;
-		case "-":
-			result = num1-num2;
-			break;
-		case "*":
-			result = num1*num2;
-			break;
-		case "/":
-			result = num1/num2;
-			break;
-		default:
-			System.out.println("Invalid Operator");
-		
+
+		Scanner scan = new Scanner(System.in); // \n\n
+
+		while (true) {  // if user never says no, then we need to make sure the loop can run infinite times
+			System.out.println("Enter your first number");
+			int f = scan.nextInt(); // 10
+
+			System.out.println("Enter your second number");
+			int s = scan.nextInt(); // 20
+
+			System.out.println("Enter a math operator");
+			char operator = scan.next().charAt(0); // +, -, *, /
+
+			switch (operator) {  //calculate the result based on the operator
+				case '-': System.out.println("Subtraction: "+(f-s));
+						 break;
+						 
+				case '+': System.out.println("Addition: "+(f+s));
+				 		break;
+				 			
+				case '*': System.out.println("Multiplication: "+(f*s));
+	 					break;
+	 					
+				case '/': System.out.println("Division: "+(f/s));
+	 					break;
+				
+	 			default:
+	 				System.out.println("Invalid Operator");
+
+			}
+
+			scan.nextLine(); // \n\n , TAKES EVERYTHING, INCLUDING ENTER
+
+			System.out.println("Do you want to continue?");
+			String a = scan.nextLine().toLowerCase();
+
+			while (!(a.equals("yes") || a.equals("no"))) { // while the answer is invalid, we continue to ask user to re-enter
+				System.out.println("Invalid Entry, pelase re-enter");
+				a = scan.nextLine().toLowerCase();
+			}// to make sure user enters yes, or no
+
+			if (a.equals("no")) {  // if the users no, then exit the loop
+				System.out.println("Test Completed");
+				break;
+			}
+
 		}
 		
-		System.out.println("Result is " + result);
+
+		scan.close();
 
 	}
 
 }
+
+/*
+ * 4. write a program that ill ask user to enter two numbers and calculates the
+ * sum of those two numbers. then it asks the user if he/she wants to continue
+ * 
+ * if the answer is yes, repeat the whole process again if the answer is no,
+ * only return the addition
+ * 
+ * but WHILE the answer is neither yes nor no, the the program should repeatedly
+ * ask the user re-enter
+ * 
+ * HINT: nested loop task, outter loop need to be an infinite loop
+ */
