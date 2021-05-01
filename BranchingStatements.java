127999f76154f059bd3a2ca805bf5d650891cf54
Yasin Kocar <77811733+ykocar@users.noreply.github.com>
2021-05-01 15:33:16 +0000

    Add files via upload

--- BranchingStatements.java
+++ BranchingStatements.java
@@ -1,57 +1,22 @@
-package week3;
-
-import java.util.Iterator;
+package week6;
 
 public class BranchingStatements {
-	
+
 	public static void main(String[] args) {
-		
 	
+		System.out.println("Test Started");
+	}
 	
-	for(int i = 1; i <= 10; i++) {
-		System.out.println(i);
-		if(i == 5) {
-			break;  // exits the current loop
+	public static void method1() {
+		for(int i=0;i<=5;i++) {
+			System.out.println("Hello World");
 		}
 	}
 	
-	System.out.println("---------------------------------------------");
-	
-	for(int j = 10; j <= 100; j+= 10) {
-		if( j == 50 || j == 70) {
-			continue;  // 
-		}
-		
-		System.out.println(j);
-	}
-	
-	System.out.println("-------------------------------------------");
-	
-	for(int m = 1; m <= 21; m++) {
-		if(m%3 ==0) {
-			continue; // skips
-		}
-		
-		System.out.print(m +" "); //1 2 4 5 7 ...
-	}
-	
-	System.out.println("\n-------------------------------------------");
-	
-	for(int k = 1; k <= 18; k++) {
-		if(k %2 != 0 ) {
-			continue;
-		}
-		
-		if(k > 12) {
-			//return;  // exits the current method
-			break; //exits the current loop
-		}
-		System.out.print(k+" ");
-		
-	}
-	
-	
-	System.out.println("\nHello EU 5");
-	}
+	/*
+	 continue: skip the iteration
+	 break: exits the loop
+	 return: terminates everything
+	 */
 
 }
