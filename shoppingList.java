bb0e878e034d99c6460e4cbcc1af57aa549d87f9
Yasin K <77811733+ykocar@users.noreply.github.com>
2021-05-11 00:24:15 +0100

    Added by Yasin
    ArrayList Practice

--- shoppingList.java
+++ shoppingList.java
@@ -1,17 +1,46 @@
-package day24_Arrays_Part1;
+package day31_ArrayList;
+
+import java.util.ArrayList;
 
 public class shoppingList {
 
 	public static void main(String[] args) {
-	
-		String[] shoppingList = {"cheese","pumpkin","bread","eggs","milk","almonds"};
-		
-		for(int i=0;i<shoppingList.length;i++) {
-						
-			System.out.print(shoppingList[i]);
-		}
 
+		ArrayList<String> shoppingList = new ArrayList<>();
+
+		shoppingList.add("paper towel");
+		shoppingList.add("dish soap");
+		shoppingList.add("trash bag");
+		shoppingList.add("clorox");
+		shoppingList.add("gloves");
+		shoppingList.add("shovel");
+
+		// print numbers of items
+
+		System.out.println(shoppingList.size());
+
+		// print all items in single line
+
+		System.out.println(shoppingList.toString());
+
+		// print first and last item in single line
+
+		System.out.println(shoppingList.get(0) + " " + shoppingList.get(shoppingList.size() - 1));
+
+		shoppingList.remove("paper towel");
+
+		System.out.println(shoppingList.toString());
+
+		shoppingList.remove(0);
+
+		System.out.println(shoppingList.toString());
+		
+		//removes all items at once
+		
+		shoppingList.clear();
+		
+		System.out.println(shoppingList.toString());
+		
 	}
 
-		
-	}
+}
