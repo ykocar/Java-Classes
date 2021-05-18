7b0ce2f64335e37ba503def5c144749e101e496a
Yasin K <77811733+ykocar@users.noreply.github.com>
2021-05-18 00:43:18 +0000

    Added by Yasin
    ArrayList Methods

---
+++ RemoveAll.java
@@ -0,0 +1,33 @@
+package day32_ArrayList_Part2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveAll {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> list1 = new ArrayList<>();
+		list1.add(30);
+		list1.add(12);
+		list1.add(22);
+
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		list2.add(30);
+		list2.add(345);
+		list2.add(1);
+		list2.add(12);
+		list2.add(22);
+		list2.add(12);
+
+		System.out.println(list1);
+		System.out.println(list2);
+
+		// removes all matching values from list1
+		list2.removeAll(Arrays.asList(345));
+		list2.removeAll(list1);
+		System.out.println(list2);
+
+	}
+
+}
